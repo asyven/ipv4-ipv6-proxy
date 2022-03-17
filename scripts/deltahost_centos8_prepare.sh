dffefd782dfddd0fb461d9f9fe8d58e78d3afd81
Yevhenii <asyven3@gmail.com>
2022-03-17 18:44:39 +0200

    Update deltahost_centos8_prepare.sh

--- scripts/deltahost_centos8_prepare.sh
+++ scripts/deltahost_centos8_prepare.sh
@@ -7,12 +7,12 @@ read IPV6GATEWAY
 main_interface=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
 echo ${main_interface}
 
-echo IPV6INIT="yes" >> /etc/sysconfig/network-scripts/ifcfg-ens3
-echo IPV6_AUTOCONF="no" >> /etc/sysconfig/network-scripts/ifcfg-ens3
-echo IPV6_DEFROUTE="yes" >> /etc/sysconfig/network-scripts/ifcfg-ens3
-echo IPV6_FAILURE_FATAL="no" >> /etc/sysconfig/network-scripts/ifcfg-ens3
-echo IPV6ADDR="\"${IPV6SUBNET}\"" >> /etc/sysconfig/network-scripts/ifcfg-ens3
-echo IPV6_DEFAULTGW="\"${IPV6GATEWAY}\"" >> /etc/sysconfig/network-scripts/ifcfg-ens3
+echo IPV6INIT="yes" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo IPV6_AUTOCONF="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo IPV6_DEFROUTE="yes" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo IPV6_FAILURE_FATAL="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo IPV6ADDR="\"${IPV6SUBNET}\"" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo IPV6_DEFAULTGW="\"${IPV6GATEWAY}\"" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
 # echo NM_CONTROLLED="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
 sudo systemctl start NetworkManager.restart
 ifdown ens3
