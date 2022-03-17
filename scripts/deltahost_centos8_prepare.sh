2810b73afbf0f9d32d1dacb0556695ef02222686
Yevhenii <asyven3@gmail.com>
2022-03-17 20:06:26 +0200

    Update deltahost_centos8_prepare.sh

--- scripts/deltahost_centos8_prepare.sh
+++ scripts/deltahost_centos8_prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "PUT SUBNET (EXPAMPLE: 2a04:a2sa:2:1::/32)"
-read IPV6SUBNET
+echo "PUT First IPv6 address: (EXPAMPLE: 2a04:a2sa:2:1::)"
+read IPV6ADDR
 echo "PUT GATEWAY (EXPAMPLE: 2a04:6ac1::2)"
 read IPV6GATEWAY
 
@@ -11,8 +11,5 @@ echo IPV6INIT="yes" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
 echo IPV6_AUTOCONF="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
 echo IPV6_DEFROUTE="yes" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
 echo IPV6_FAILURE_FATAL="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
-echo IPV6ADDR="\"${IPV6SUBNET}\"" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
-echo IPV6_DEFAULTGW="\"${IPV6GATEWAY}\"" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
-#echo NM_CONTROLLED="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
-sudo systemctl start NetworkManager.restart
-#ifup eth0
+echo IPV6ADDR="${IPV6ADDR}/32" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo IPV6_DEFAULTGW="${IPV6GATEWAY}" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
