cfef6692151662e1e39d1ae6fe49abb81fc7d93c
Yevhenii <asyven3@gmail.com>
2022-03-17 18:05:37 +0200

    Create deltahost_centos8_prepare.sh

---
+++ scripts/deltahost_centos8_prepare.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "PUT SUBNET (EXPAMPLE: 2a04:a2sa:2:1::/32)"
+read IPV6SUBNET
+echo "PUT GATEWAY (EXPAMPLE: 2a04:6ac1::2)"
+read IPV6GATEWAY
+
+main_interface=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
+ifdown ${main_interface}
+sudo systemctl stop NetworkManager.service
+echo IPV6INIT="yes" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo IPV6_AUTOCONF="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo IPV6_DEFROUTE="yes" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo IPV6_FAILURE_FATAL="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo IPV6ADDR="${IPV6SUBNET}" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo IPV6_DEFAULTGW="${IPV6GATEWAY}" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+echo NM_CONTROLLED="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
+sudo systemctl start NetworkManager.service
+ifup ${main_interface}
+
